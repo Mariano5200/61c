59ac862e5952ca2a82035219043e6a3136c89c80
Michael Ball <cycomachead@gmail.com>
2013-02-01 19:08:43 -0800

    Almost done with HW1.

--- hw/hw1/paircount.c
+++ hw/hw1/paircount.c
@@ -4,6 +4,13 @@ int pairCount(unsigned int n);
 
 int main(int argc, char *argv[]) {
     const char testStr[] = "# pairs in base 2 of %u = %d, should be %d\n";
+    if (argc > 1) {
+      printf ("%d", pairCount(*argv[1]));
+      return 0;
+    } else if (argc > 2) {
+      printf ("too many arguments!");
+      return 0;
+    }
     printf (testStr, 0, pairCount (0), 0);
     printf (testStr, 11, pairCount (11), 1);
     printf (testStr, 2863377066u, pairCount (2863377066u), 2);
@@ -12,5 +19,26 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+// Use << or >>
+int pairCountBitWise(unsigned int n) {
+  // int len = sizeof(n);
+  int elevCount = 0;
+  // int curr = 0;
+  // use a for-loop and bit shift the number.
+  // for(int i = 0; i < len; i += 1) {}
+  return elevCount;
+}
+
+// Use Div by 2 algo to conver to binary.
 int pairCount(unsigned int n) {
+  int elevCount = 0;
+  int remainprev = 0;
+  int remaincurr = 0;
+  while (n > 0) {
+    remainprev = remaincurr;
+    remaincurr = n % 2;
+    n /= 2;
+    elevCount += ((remaincurr == 1) & (remainprev == 1)) ? 1 : 0;
+  }
+  return elevCount;
 }
