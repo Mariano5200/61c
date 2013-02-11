7700c275b1081eb66db8c175dae1543f52cd42f3
Michael Ball <cycomachead@gmail.com>
2013-02-10 23:46:28 -0800

    hw2 sucks

--- hw/hw2/matcher.c
+++ hw/hw2/matcher.c
@@ -19,37 +19,48 @@ int isSpecial(char c) {
     return c == '.' || c == '*' || c == '\\';
 }
 
-int matcher(char *ln, char *pat, int lnPos, int patPos, int maxLn, int maxPat);
+int matcher(int lnPos, int patPos, int maxLn, int maxPat);
 
+char *ln;
+char *pat;
 int rgrep_matches(char *line, char *pattern) {
-    return matcher(line, pattern, 0, 0, strlen(line), strlen(pattern));
+    pat = pattern;
+    ln = line;
+    return matcher(0, 0, strlen(line), strlen(pattern));
 }
 
 
-int matcher(char *ln, char *pat, int lnPos, int patPos, int maxLn, int maxPat) {
-
+int matcher(int lnPos, int patPos, int maxLn, int maxPat) {
     // We've exceeded the limit of either the line or the pattern.
     if (lnPos >= maxLn && patPos < maxPat) { // chars left in pattern. ???
-        return pat[patPos] == '*' ? 1 : 0;
+        if (pat[patPos] == '*') {
+            return matcher(lnPos - 1, patPos + 1, maxLn, maxPat);
+        } else {
+            return 0;
+        }
     } else if (patPos >= maxPat && lnPos < maxLn) { // finished the pattern
         return 1;
     } else if (lnPos >= maxLn && patPos >= maxPat) { // finished both
         return 1;
-    } else if (pat[patPos] == '*') {
-        if (pat[patPos - 1] == ln[lnPos] || pat[patPos - 1] == '.') {
-            return matcher(ln, pat, lnPos + 1, patPos, maxLn, maxPat);
+    } else if (pat[patPos] == '*') { // \* doesn't work here...
+        if (pat[patPos - 1] == ln[lnPos] && pat[patPos - 1] != '\\') {
+            return matcher(lnPos + 1, patPos, maxLn, maxPat) || \
+                matcher(lnPos - 1, patPos + 1, maxLn, maxPat) || \
+                matcher(lnPos - 1, patPos, maxLn, maxPat) || \
+                matcher(lnPos, patPos + 1, maxLn, maxPat);
+            // this is a really hacky problem for a*a.
         } else {
-            return matcher(ln, pat, lnPos + 1, patPos + 1, maxLn, maxPat);
+            return matcher(0, patPos + 1, maxLn, maxPat);
         }
     } else if (pat[patPos] == '\\') {
         if (pat[patPos + 1] == ln[lnPos]) {
-            return matcher(ln, pat, lnPos + 1, patPos + 1, maxLn, maxPat);
+            return matcher(lnPos + 1, patPos + 1, maxLn, maxPat);
         } else {
-            return matcher(ln, pat, lnPos + 1, patPos, maxLn, maxPat);
+            return matcher(lnPos + 1, patPos, maxLn, maxPat);
         }
     } else if (ln[lnPos] == pat[patPos] || pat[patPos] == '.') { // correct.
-        return matcher(ln, pat, lnPos + 1, patPos + 1, maxLn, maxPat);
+        return matcher(lnPos + 1, patPos + 1, maxLn, maxPat);
     } else {
-        return matcher(ln, pat, lnPos + 1, patPos, maxLn, maxPat);
+        return matcher(lnPos + 1, patPos, maxLn, maxPat);
     }
 }
