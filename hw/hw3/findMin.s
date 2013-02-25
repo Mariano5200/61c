b2af4934b564d030618716be5f6b05dbf65570af
Class_Account <cs61c-mx@hive16.CS.Berkeley.EDU>
2013-02-25 14:09:50 -0800

    HW3 init

---
+++ hw/hw3/findMin.s
@@ -0,0 +1,38 @@
+	.data
+# Do not modify the following linked list
+L9: 	.word 23 0
+L7: 	.word 100 L8
+L6: 	.word 43 L7
+L5: 	.word 24 L6
+L8: 	.word 21 L9
+L3: 	.word 15 L4
+L4: 	.word 43 L5
+L2: 	.word -2 L3
+L1: 	.word 21 L2
+L0: 	.word 20 L1
+space:
+	.ascii " "
+	
+	.text 
+	la $a0 L8 # find min from node 8
+	jal findMin
+	move $a0 $v0
+	li $v0 1
+	syscall
+        
+	la $a0 space
+	li $v0 4
+	syscall
+        
+	la $a0 L0 # find min from node 0
+	jal findMin
+	move $a0 $v0
+	li $v0 1
+	syscall
+	li $v0 10
+	syscall
+	
+# Your code here
+# You must use recursion for full credit 
+findMin:
+	jr $ra
