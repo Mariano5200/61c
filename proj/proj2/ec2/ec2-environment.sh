cbf1b117d9ea22ef89cfbb81c22603fbc02fffb0
Class_Account <cs61c-mx@hive15.CS.Berkeley.EDU>
2013-03-22 16:41:26 -0700

    ec2 begin

---
+++ proj/proj2/ec2/ec2-environment.sh
@@ -0,0 +1,15 @@
+# Run this file with source or '.'
+AWS_ACCESS_KEY_ID=AKIAJ3G2754RV7QNYC5A
+AWS_SECRET_ACCESS_KEY=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
+JAVA_HOME=${JAVA_HOME-/usr/lib/jvm/java-6-openjdk/jre}
+AWS_IAM_HOME=/home/ff/cs61c/aws/IAMCli
+AWS_CREDENTIAL_FILE=/home/cc/cs61c/sp13/class/cs61c-mx/.aws-creds
+EC2_PRIVATE_KEY=/home/cc/cs61c/sp13/class/cs61c-mx/.aws-cert-private.pem
+EC2_CERT=/home/cc/cs61c/sp13/class/cs61c-mx/.aws-cert-public.pem
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
+#export JAVA_HOME
+export AWS_IAM_HOME
+export AWS_CREDENTIAL_FILE
+export EC2_PRIVATE_KEY
+export EC2_CERT
