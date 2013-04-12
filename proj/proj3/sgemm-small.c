1f90be5bcdb84784dcb8b1ee833cc8d0e09019ed
Michael Ball <cycomachead@gmail.com>
2013-04-12 10:29:50 -0700

    clean up, Make restore, .gitignore.

--- proj/proj3/sgemm-small.c
+++ proj/proj3/sgemm-small.c
@@ -9,20 +9,20 @@ void sgemm( int m, int n, int d, float *A, float *C )
             __m128 a1;
             a1 = _mm_load1_ps(A+j*(n+1)+k*(n));
             int i;
-            #pragma vectorize unroll optimize("", on)
             int ntmp = n/8*8;
+            #pragma vectorize optimize("", on)
             for(i = 0; i < ntmp; i += 8 ) {
-                int *ld = A+i+k*(n);
-                int *ad = C+i+j*n;
-                __m128 a2 = _mm_loadu_ps(ld);
-                __m128 a3 = _mm_loadu_ps(ld+4);
+                int *ikn = A+i+k*(n);
+                int *ijn = C+i+j*n;
+                __m128 a2 = _mm_loadu_ps(ikn);
+                __m128 a3 = _mm_loadu_ps(ikn+4);
                 __m128 mulres = _mm_mul_ps(a1, a2);
                 __m128 mulres2 = _mm_mul_ps(a1, a3);
-                __m128 sum = _mm_add_ps(_mm_loadu_ps(ad), mulres);
-                __m128 sum3 = _mm_add_ps(_mm_loadu_ps(ad+4), mulres2);
+                __m128 sum = _mm_add_ps(_mm_loadu_ps(ijn), mulres);
+                __m128 sum3 = _mm_add_ps(_mm_loadu_ps(ijn+4), mulres2);
 
-                _mm_storeu_ps(ad, sum);
-                _mm_storeu_ps(ad+4, sum3);
+                _mm_storeu_ps(ijn, sum);
+                _mm_storeu_ps(ijn+4, sum3);
             }
             for (; i < n; i++) {
                 C[i+j*n] += A[i+k*n] * A[j*(n+1)+k*n];
