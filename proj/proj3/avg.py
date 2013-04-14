363035a8dae56e277797415082029b1e1221a78a
Michael Ball <cycomachead@gmail.com>
2013-04-13 18:26:36 -0700

    stuff

--- proj/proj3/avg.py
+++ proj/proj3/avg.py
@@ -6,15 +6,17 @@ import re
 
 if (len(sys.argv) >= 2):
      runs = int(sys.argv[1])
+     file = str(sys.argv[2])
 else:
     runs = 5
+    file = ""
 total = 0
 
 pat = re.compile("[0-9]*\.?[0-9]*")
 if __name__ == '__main__':
     run = 0
     while (run < runs):
-        proc = subprocess.Popen('./bench-small', stdout=subprocess.PIPE)
+        proc = subprocess.Popen(file, stdout=subprocess.PIPE)
         result = proc.communicate()[0].decode("utf-8")
         curr = pat.match(result).group()
         print("Run {0}: {1} Gflops".format(run, curr))
--- proj/proj3/sgemm-small.c
+++ proj/proj3/sgemm-small.c
@@ -9,31 +9,32 @@ void sgemm( int m, int n, int d, float *A, float *C )
     if (n == 40) {
         int jn, kn, i, j, k, upb;
         upb = (n/20)*20;
-        __m128 c1,c2,c3,c4,c5,a11,a12,a13,a14,a15,a2;
-        // __m128 temp1,temp2,temp3,temp4,temp5;
-        float *t, *r, *p;
+        __m128 c1,c2,c3,c4,c5,temp1,temp2,temp3,temp4,temp5,a11,a12,a13,a14,a15,a2;
+        float *t;
+        float *r;
+        float *p;
         for(j = 0; j < n; j++ ) {
             jn = j*n;
             for(i = 0; i < upb; i+=20 ) {
-                p = C+i+jn;
-                c1 = _mm_loadu_ps(p);
-                c2 = _mm_loadu_ps(p+4);
-                c3 = _mm_loadu_ps(p+8);
-                c4 = _mm_loadu_ps(p+12);
-                c5 = _mm_loadu_ps(p+16);
+            p = C+i+jn;
+            c1 = _mm_loadu_ps(p);
+            c2 = _mm_loadu_ps(p+4);
+            c3 = _mm_loadu_ps(p+8);
+            c4 = _mm_loadu_ps(p+12);
+            c5 = _mm_loadu_ps(p+16);
 
-                for(k = 0; k < m; k++ ) {
-                    kn=k*n;
-                    t = A+(i+kn);
-                    r = A+(jn+j+kn);
-                    //C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
-                    a11 = _mm_loadu_ps(t);
-                    a12 = _mm_loadu_ps(t+4);
-                    a13 = _mm_loadu_ps(t+8);
-                    a14 = _mm_loadu_ps(t+12);
-                    a15 = _mm_loadu_ps(t+16);
+            for(k = 0; k < m; k++ ) {
+                kn=k*n;
+                t = A+(i+kn);
+                r = A+(jn+j+kn);
+                //C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
+                a11 = _mm_loadu_ps(t);
+                a12 = _mm_loadu_ps(t+4);
+                a13 = _mm_loadu_ps(t+8);
+                a14 = _mm_loadu_ps(t+12);
+                a15 = _mm_loadu_ps(t+16);
 
-                    a2 = _mm_load1_ps(r);
+                a2 = _mm_load1_ps(r);
 
                 /*temp1 = _mm_mul_ps(a11,a2);
                   temp2 = _mm_mul_ps(a12,a2);
@@ -57,7 +58,7 @@ void sgemm( int m, int n, int d, float *A, float *C )
 
             }
         }
-    } else if(n%8==0){
+    } else if(n%8==0) {
     int jn, kn, i, j, k, upb;
     //upb = (n/20)*20;
     __m128 c1,c2,c3,c4,c5,temp1,temp2,temp3,temp4,temp5,a11,a12,a13,a14,a15,a2;
@@ -97,59 +98,59 @@ void sgemm( int m, int n, int d, float *A, float *C )
         }
     }
     } else if(n%8 == 4) {
-        int jn, kn, i, j, k, upb;
-        //upb = (n/20)*20;
-        __m128 c1,c2,c3,c4,c5,temp1,temp2,temp3,temp4,temp5,a11,a12,a13,a14,a15,a2;
-        float *t;
-        float *r;
-        float *p;
-        upb = (n/8)*8;
-        for(j = 0; j < n; j++ ) {
-            jn = j*n;
-            for(i = 0; i < upb; i+=8 ) {
-            p = C+i+jn;
-            c1 = _mm_loadu_ps(p);
-            c2 = _mm_loadu_ps(p+4);
+    int jn, kn, i, j, k, upb;
+    //upb = (n/20)*20;
+    __m128 c1,c2,c3,c4,c5,temp1,temp2,temp3,temp4,temp5,a11,a12,a13,a14,a15,a2;
+    float *t;
+    float *r;
+    float *p;
+    upb = (n/8)*8;
+    for(j = 0; j < n; j++ ) {
+        jn = j*n;
+        for(i = 0; i < upb; i+=8 ) {
+        p = C+i+jn;
+        c1 = _mm_loadu_ps(p);
+        c2 = _mm_loadu_ps(p+4);
 
-            for(k = 0; k < m; k++ ) {
-                kn=k*n;
-                t = A+(i+kn);
-                r = A+(jn+j+kn);
-                //C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
-                a11 = _mm_loadu_ps(t);
-                a12 = _mm_loadu_ps(t+4);
+        for(k = 0; k < m; k++ ) {
+            kn=k*n;
+            t = A+(i+kn);
+            r = A+(jn+j+kn);
+            //C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
+            a11 = _mm_loadu_ps(t);
+            a12 = _mm_loadu_ps(t+4);
 
-                a2 = _mm_load1_ps(r);
+            a2 = _mm_load1_ps(r);
 
-                /*temp1 = _mm_mul_ps(a11,a2);
-                  temp2 = _mm_mul_ps(a12,a2);
-                  temp3 = _mm_mul_ps(a13,a2);
-                  temp4 = _mm_mul_ps(a14,a2);
-                  temp5 = _mm_mul_ps(a15,a2);*/
+            /*temp1 = _mm_mul_ps(a11,a2);
+              temp2 = _mm_mul_ps(a12,a2);
+              temp3 = _mm_mul_ps(a13,a2);
+              temp4 = _mm_mul_ps(a14,a2);
+              temp5 = _mm_mul_ps(a15,a2);*/
 
 
-                c1 = _mm_add_ps(_mm_mul_ps(a11,a2),c1);
-                c2 = _mm_add_ps(_mm_mul_ps(a12,a2),c2);
+            c1 = _mm_add_ps(_mm_mul_ps(a11,a2),c1);
+            c2 = _mm_add_ps(_mm_mul_ps(a12,a2),c2);
 
-            }
-            _mm_storeu_ps(p, c1);
-            _mm_storeu_ps(p+4,c2);
-            }
-            p = C+i+jn;
-            c1 = _mm_loadu_ps(p);
-            for (k =0; k < m; k ++) {
-                kn=k*n;
-                t = A+(i+kn);
-                r = A+(jn+j+kn);
-                //C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
-                a11 = _mm_loadu_ps(t);
-
-                a2 = _mm_load1_ps(r);
-                c1 = _mm_add_ps(_mm_mul_ps(a11,a2),c1);
-
-            }
-            _mm_storeu_ps(p, c1);
         }
+        _mm_storeu_ps(p, c1);
+        _mm_storeu_ps(p+4,c2);
+        }
+        p = C+i+jn;
+        c1 = _mm_loadu_ps(p);
+        for (k =0; k < m; k ++) {
+        kn=k*n;
+        t = A+(i+kn);
+        r = A+(jn+j+kn);
+        //C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
+        a11 = _mm_loadu_ps(t);
+
+        a2 = _mm_load1_ps(r);
+        c1 = _mm_add_ps(_mm_mul_ps(a11,a2),c1);
+
+        }
+        _mm_storeu_ps(p, c1);
+    }
     } else if (n%8 < 4) {
     int jn, kn, i, j, k, upb;
     upb = (n/8)*8;
@@ -235,21 +236,22 @@ void sgemm( int m, int n, int d, float *A, float *C )
         p = C+i+jn;
         c1 = _mm_loadu_ps(p);
         for (k =0; k < m; k ++) {
-            kn=k*n;
-            t = A+(i+kn);
-            r = A+(jn+j+kn);
-            //C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
-            a11 = _mm_loadu_ps(t);
+        kn=k*n;
+        t = A+(i+kn);
+        r = A+(jn+j+kn);
+        //C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
+        a11 = _mm_loadu_ps(t);
 
-            a2 = _mm_load1_ps(r);
-            c1 = _mm_add_ps(_mm_mul_ps(a11,a2),c1);
+        a2 = _mm_load1_ps(r);
+        c1 = _mm_add_ps(_mm_mul_ps(a11,a2),c1);
+
         }
         _mm_storeu_ps(p, c1);
         i += 4;
         for (; i < n; i ++) {
-            for (k = 0; k < m; k++) {
-                C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
-            }
+        for (k = 0; k < m; k++) {
+            C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
+        }
         }
     }
     }
