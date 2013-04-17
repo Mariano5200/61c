6761c0ec1c18c54cf8db63c723b4d62e42e943a7
Michael Ball <cycomachead@gmail.com>
2013-04-17 01:11:49 -0700

    parallel test

--- proj/proj3/sgemm-openmp.c
+++ proj/proj3/sgemm-openmp.c
@@ -17,10 +17,10 @@ void sgemm( int m, int n, int d, float *A, float *C )
         float *t;
         float *r;
         float *p;
-		#pragma omp parallel for private(j)
+        // #pragma omp parallel for private(j)
         for(j = 0; j < n; j++ ) {
             jn = j*n;
-            // #pragma omp parallel for
+            #pragma omp for private(i)
             for(i = 0; i < upb; i+=20 ) {
                 p = C+i+jn;
                 c1 = _mm_loadu_ps(p);
@@ -28,7 +28,7 @@ void sgemm( int m, int n, int d, float *A, float *C )
                 c3 = _mm_loadu_ps(p+8);
                 c4 = _mm_loadu_ps(p+12);
                 c5 = _mm_loadu_ps(p+16);
-                // #pragma omp parallel for
+                #pragma omp for private(k)
                 for(k = 0; k < m; k++ ) {
                     kn=k*n;
                     t = A+(i+kn);
@@ -63,15 +63,15 @@ void sgemm( int m, int n, int d, float *A, float *C )
         float *t;
         float *r;
         float *p;
-#pragma omp parallel for private(j)
+        // #pragma omp parallel for private(j)
         for(j = 0; j < n; j++ ) {
             jn = j*n;
-            // #pragma omp parallel for
+#pragma omp for private(i)
             for(i = 0; i < n; i+=8 ) {
                 p = C+i+jn;
                 c1 = _mm_loadu_ps(p);
                 c2 = _mm_loadu_ps(p+4);
-                // #pragma omp parallel for
+                #pragma omp for private(k)
                 for(k = 0; k < m; k++ ) {
                     kn=k*n;
                     t = A+(i+kn);
@@ -98,15 +98,15 @@ void sgemm( int m, int n, int d, float *A, float *C )
         float *r;
         float *p;
         upb = (n/8)*8;
-#pragma omp parallel for private(j)
+        // #pragma omp parallel for private(j)
         for(j = 0; j < n; j++ ) {
             jn = j*n;
-            // #pragma omp parallel for
+            #pragma omp for private(i)
             for(i = 0; i < upb; i+=8 ) {
                 p = C+i+jn;
                 c1 = _mm_loadu_ps(p);
                 c2 = _mm_loadu_ps(p+4);
-                // #pragma omp parallel for
+                #pragma omp for private(k)
                 for(k = 0; k < m; k++ ) {
                     kn=k*n;
                     t = A+(i+kn);
@@ -125,35 +125,35 @@ void sgemm( int m, int n, int d, float *A, float *C )
             }
             p = C+i+jn;
             c1 = _mm_loadu_ps(p);
-            // #pragma omp parallel for
-            for (k =0; k < m; k ++) {
-                kn=k*n;
-                t = A+(i+kn);
-                r = A+(jn+j+kn);
-                //C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
-                a11 = _mm_loadu_ps(t);
+            #pragma omp for private(k)
+            for(k = 0; k < m; k++ ) {
+            kn=k*n;
+            t = A+(i+kn);
+            r = A+(jn+j+kn);
+            //C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
+            a11 = _mm_loadu_ps(t);
 
-                a2 = _mm_load1_ps(r);
-                c1 = _mm_add_ps(_mm_mul_ps(a11,a2),c1);
+            a2 = _mm_load1_ps(r);
+            c1 = _mm_add_ps(_mm_mul_ps(a11,a2),c1);
             }
             _mm_storeu_ps(p, c1);
         }
-    } else if (n%8 < 4) {
+    } else {
         int jn, kn, i, j, k, upb;
         upb = (n/8)*8;
         __m128 c1,c2,c3,c4,c5,a11,a12,a13,a14,a15,a2;
         float *t;
         float *r;
         float *p;
-#pragma omp parallel for private(j)
+        // #pragma omp parallel for private(j)
         for(j = 0; j < n; j++ ) {
             jn = j*n;
-            // #pragma omp parallel for
+            // #pragma omp for private(i)
             for(i = 0; i < upb; i+=8 ) {
                 p = C+i+jn;
                 c1 = _mm_loadu_ps(p);
                 c2 = _mm_loadu_ps(p+4);
-                // #pragma omp parallel for
+                #pragma omp for private(k)
                 for(k = 0; k < m; k++ ) {
                     kn=k*n;
                     t = A+(i+kn);
@@ -170,69 +170,16 @@ void sgemm( int m, int n, int d, float *A, float *C )
                 _mm_storeu_ps(p, c1);
                 _mm_storeu_ps(p+4,c2);
             }
-            // #pragma omp parallel for
+            // #pragma omp for private(i)
             for (i = i; i < n; i ++) {
-                // #pragma omp parallel for
-                for (k = 0; k < m; k++) {
-                    C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
-                }
-            }
-        }
-    } else if(n%8 > 4) {
-        int jn, kn, i, j, k, upb;
-        upb = (n/8)*8;
-        __m128 c1,c2,c3,c4,c5,temp1,temp2,temp3,temp4,temp5,a11,a12,a13,a14,a15,a2;
-        float *t;
-        float *r;
-        float *p;
-#pragma omp parallel for private(j)
-        for(j = 0; j < n; j++ ) {
-            jn = j*n;
-            //#pragma omp parallel for
-            for(i = 0; i < upb; i+=8 ) {
-                p = C+i+jn;
-                c1 = _mm_loadu_ps(p);
-                c2 = _mm_loadu_ps(p+4);
-                // #pragma omp parallel for
+                #pragma omp for private(k)
                 for(k = 0; k < m; k++ ) {
-                    kn=k*n;
-                    t = A+(i+kn);
-                    r = A+(jn+j+kn);
-                    //C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
-                    a11 = _mm_loadu_ps(t);
-                    a12 = _mm_loadu_ps(t+4);
-
-                    a2 = _mm_load1_ps(r);
-
-                    c1 = _mm_add_ps(_mm_mul_ps(a11,a2),c1);
-                    c2 = _mm_add_ps(_mm_mul_ps(a12,a2),c2);
-                }
-                _mm_storeu_ps(p, c1);
-                _mm_storeu_ps(p+4,c2);
-            }
-            p = C+i+jn;
-            c1 = _mm_loadu_ps(p);
-            // #pragma omp parallel for
-            for (k =0; k < m; k ++) {
-                kn=k*n;
-                t = A+(i+kn);
-                r = A+(jn+j+kn);
-                //C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
-                a11 = _mm_loadu_ps(t);
-
-                a2 = _mm_load1_ps(r);
-                c1 = _mm_add_ps(_mm_mul_ps(a11,a2),c1);
-            }
-            _mm_storeu_ps(p, c1);
-            i += 4;
-            //#pragma omp parallel for
-            for (i = i ; i < n; i ++) {
-                // #pragma omp parallel for
-                for (k = 0; k < m; k++) {
                     C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
                 }
             }
         }
     }
+    
     }
+
 }
