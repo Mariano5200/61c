191145c4fca6d809d44dd39547f70c7b7d088146
Michael Ball <cycomachead@gmail.com>
2013-04-19 02:00:20 -0700

    avg optimizations

--- proj/proj3/test/avg.py
+++ proj/proj3/test/avg.py
@@ -5,8 +5,8 @@ import subprocess
 import re
 
 if (len(sys.argv) >= 2):
-     runs = int(sys.argv[1])
-     file = str(sys.argv[2])
+, runs = int(sys.argv[1])
+, file = str(sys.argv[2])
 else:
     runs = 5
     file = ""
@@ -17,27 +17,38 @@ def avg(l):
 
 all_runs = {}
 pat = re.compile("[0-9]*\.?[0-9]*")
+n_sizes = [400,533,711,948,1264,512,768,1024,1000,800,
+600,439,499,571,829,859,907,1213,1333,1499,700,1300,1111]
+#bounded 400,1500
+n_sizes.sort()
+m_sizes = [32,100,64,48,97,37,43,53,61,78,83,50] #bounded 32,100
+m_sizes.sort()
+bs_sizes = [400,800,1600,512,1024,750,1000,1200,500,700,
+1200,1400]
+bs_sizes.sort()
+
+
+
 if __name__ == '__main__':
-    for bs in range(100,1600, 50):
-        if bs not in [100, 150,300]:
-            for n in range(400,1501, 50):
-                for m in range(32, 100, 2):
-                    total = 0
-                    run = 0
-                    while (run < runs):
-                        proc = subprocess.Popen([file,
-                         str(n), str(m), str(bs)], stdout=subprocess.PIPE)
-                        result = proc.communicate()[0].decode("utf-8")
-                        curr = pat.match(result).group()
-                        print("Run {0}: N: {1}, M: {2}, BS: {3}\t{4} Gflops".format(run, n, m, bs, curr))
-                        total += float(curr)
-                        run += 1
-                    total /= runs
-                    print("\nAveraged: \t" + str(total) + " Gflops")
-                    if bs not in all_runs:
-                        all_runs[bs] = [total]
-                    else:
-                        all_runs[bs] += [total]
+    for bs in bs_sizes:
+        for n in n_sizes:
+            for m in m_sizes:
+                total = 0
+                run = 0
+                while (run < runs):
+                    proc = subprocess.Popen([file,
+                    str(n), str(m), str(bs)], stdout=subprocess.PIPE)
+                    result = proc.communicate()[0].decode("utf-8")
+                    curr = pat.match(result).group()
+                    print("Run {0}: N: {1}, M: {2}, BS: {3}\t{4} Gflops".format(run, n, m, bs, curr))
+                    total += float(curr)
+                    run += 1
+                total /= runs
+                print("Averaged: \t\t\t\t" + str(total) + " Gflops\n")
+                if bs not in all_runs:
+                    all_runs[bs] = [total]
+                else:
+                    all_runs[bs] += [total]
     for key, value in all_runs:
         mean = avg(value)
         print("Blocksize: ", key, "\t", mean, "Gflops")
